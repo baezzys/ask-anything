dcb834c148fab26e5918d0154864a71116470bf0
kheeyaa <kheeyaa@icloud.com>
2022-09-14 23:10:52 +0900

    [feature] Update Basic Component

---
+++ client/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Ask Anything</title>
+      <meta name="description" content="Ask anything" />
+      <link rel="icon" href="/assets/images/gamza.png" />
+    </Head>
+
+    <main>
+      <div>hello</div>
+    </main>
+
+    <footer>footer</footer>
+  </div>
+);
+
+export default Home;
