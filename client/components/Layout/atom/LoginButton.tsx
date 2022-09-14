c6c5094ea56101f8452f3e0e16a7fd519898859a
kheeyaa <kheeyaa@icloud.com>
2022-09-14 23:12:39 +0900

    [feature] Add LoginButton

---
+++ client/components/Layout/atom/LoginButton.tsx
@@ -0,0 +1,32 @@
+import { css } from '@emotion/react';
+import LoginModal from 'components/common/LoginModal';
+import { useState } from 'react';
+import colors from 'utils/style/colors';
+import pxToRem from 'utils/style/pxToRem';
+
+const LoginButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const open = () => {
+    setIsOpen(true);
+  };
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <button css={buttonStyle} onClick={open} type="button">
+        로그인
+      </button>
+
+      <LoginModal isOpen={isOpen} close={close} />
+    </>
+  );
+};
+
+const buttonStyle = css`
+  color: ${colors.pink[100]};
+  padding: 0 ${pxToRem(10)};
+`;
+
+export default LoginButton;
