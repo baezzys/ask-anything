224fdfd1a6ba3a9d8512fef752689dd932ba9f05
Jinwoo Bae <wlsdn3578@gmail.com>
2022-11-13 23:25:28 +0900

    feat: Add security config to post service

--- backend/spring-post/src/main/java/com/example/post/controller/PostController.java
+++ backend/spring-post/src/main/java/com/example/post/controller/PostController.java
@@ -1,16 +1,19 @@
 package com.example.post.controller;
 
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class PostController {
 
     @PostMapping("/post")
-    public String createPost() {
-        System.out.println("Post is created");
-        return "hello";
+    public Mono<String> createPost(@RequestHeader HttpHeaders headers, @AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal) {
+        return Mono.just(principal.getAttributes().get("name").toString());
     }
 }
